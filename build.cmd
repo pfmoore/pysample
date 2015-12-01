50a90dfc1851cd0eef8f6f0b13f9bed27423eb78
Paul Moore <p.f.moore@gmail.com>
2015-12-01 09:40:38 +0000

    Use a batch file to run the build with SDK if needed

---
+++ build.cmd
@@ -0,0 +1,17 @@
+@echo off
+:: To build extensions for 64 bit Python 3, we need to configure environment
+:: variables to use the MSVC 2010 C++ compilers from GRMSDKX_EN_DVD.iso of:
+:: MS Windows SDK for Windows 7 and .NET Framework 4
+::
+:: More details at:
+:: https://github.com/cython/cython/wiki/64BitCythonExtensionsOnWindows
+
+IF "%DISTUTILS_USE_SDK%"=="1" (
+    ECHO Configuring environment to build with MSVC on a 64bit architecture
+    ECHO Using Windows SDK 7.1A
+    "C:\Program Files\Microsoft SDKs\Windows\7.1A\Bin\SetEnv.cmd" /x64 /release
+    SET MSSdk=1
+) ELSE (
+    ECHO Using default MSVC build environment
+)
+call %*
