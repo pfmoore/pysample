2b16128062a0dd9fb2d965d9f2a85d91da2705ce
Paul Moore <p.f.moore@gmail.com>
2015-12-01 10:42:10 +0000

    Try one more time...

--- build.cmd
+++ build.cmd
@@ -9,7 +9,7 @@
 IF "%DISTUTILS_USE_SDK%"=="1" (
     ECHO Configuring environment to build with MSVC on a 64bit architecture
     ECHO Using Windows SDK 7.1
-    "C:\Program Files\Microsoft SDKs\Windows\7.1\Bin\SetEnv.cmd" /x64 /release
+    "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64 /release
     SET MSSdk=1
 ) ELSE (
     ECHO Using default MSVC build environment
